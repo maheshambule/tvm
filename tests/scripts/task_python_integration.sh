77cb917f11b2bf085f21e2806aa8d0e4f093e20b
maheshambule <mahesh_ambule@persistent.com>
2019-12-09 14:20:41 +0530

    remove onnx runtime install

--- tests/scripts/task_python_integration.sh
+++ tests/scripts/task_python_integration.sh
@@ -30,12 +30,6 @@ rm -rf python/tvm/*.pyc python/tvm/*/*.pyc python/tvm/*/*/*.pyc
 make cython3
 
 
-#Install onnx & onnxruntime for testing Relay to ONNX implementation
-echo "Installing onnx and onnx runtime"
-pip3 install onnx --user
-pip3 install onnxruntime --user
-
-
 # Test extern package
 cd apps/extension
 rm -rf lib
